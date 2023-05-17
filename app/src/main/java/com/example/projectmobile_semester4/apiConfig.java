969a4ce66a121495d1bbf53e95439840148760ef
fillah23 <fillahseptian7l24@gmail.com>
2023-05-17 12:51:06 +0700

    validasi

--- app/src/main/java/com/example/projectmobile_semester4/apiConfig.java
+++ app/src/main/java/com/example/projectmobile_semester4/apiConfig.java
@@ -1,7 +1,7 @@
 package com.example.projectmobile_semester4;
 
 public class apiConfig {
-    public static final String BASE_URL = "https://dfcf-125-166-116-103.ngrok-free.app/api/";
+    public static final String BASE_URL = "https://f60c-103-109-209-254.ngrok-free.app/api/";
     public static final String LOGIN_ENDPOINT = BASE_URL + "login";
     public static final String CUSTOMERS_ENDPOINT = BASE_URL + "customers-with-product";
     public static final String TRANSACTION = BASE_URL + "transaction";
